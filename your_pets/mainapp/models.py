b1a3ec21d8fb3059684fe14923b5c135351399e8
Darina-I <147441062+Darina-I@users.noreply.github.com>
2024-02-26 18:30:26 +0500

    update models (CustomUser)

--- your_pets/mainapp/models.py
+++ your_pets/mainapp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-
+class CustomUser(AbstractUser):
+    username = models.CharField(unique=True, max_length=150, verbose_name='Логин')
