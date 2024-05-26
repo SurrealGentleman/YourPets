0d45f557c99f57f477331d44f55a1445955fde9e
SurrealGentleman <alexeev1one@mail.ru>
2024-05-27 00:52:10 +0500

    fix js->darkening in profile.html

--- your_pets/mainapp/static/js/send_request_on_add_update.js
+++ your_pets/mainapp/static/js/send_request_on_add_update.js
@@ -36,4 +36,41 @@ $(document).ready(function() {
             }
         });
     }
+
+    $('input[type="radio"]').each(function() {
+        if ($(this).is(':checked')) {
+            var petId = this.id;
+            if (petId == 'add-pet_button') {
+                $.ajax({
+                    url: '/get_form/',
+                    type: 'GET',
+                    data: {
+                        'add_pet': true
+                    },
+                    success: function(response) {
+                        $('#form-container-add_update_pet').html(response.form_html);
+                    },
+                    error: function(xhr, errmsg, err) {
+                        console.log(xhr.status + ": " + xhr.responseText);
+                    }
+                });
+            }
+            else {
+                $.ajax({
+                    url: '/get_form/',
+                    type: 'GET',
+                    data: {
+                        'update_pet': true,
+                        'pet_id': petId
+                    },
+                    success: function(response) {
+                        $('#form-container-add_update_pet').html(response.form_html);
+                    },
+                    error: function(xhr, errmsg, err) {
+                        console.log(xhr.status + ": " + xhr.responseText);
+                    }
+                });
+            }
+        }
+    });
 });
